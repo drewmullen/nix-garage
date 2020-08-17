99bc04f2f0fdfe27c2b7660e8c182d74e0bc1b84
Drew Mullen <drew@nebulaworks.com>
2020-08-17 16:40:45 -0400

    wip

--- imgs/lambda-builder-python/default.nix
+++ imgs/lambda-builder-python/default.nix
@@ -4,7 +4,7 @@ let
   pkgs = import ../../pin { snapshot = "nixos-20-03_0"; };
   lib = pkgs.lib;
   script = pkgs.writeScriptBin "lambda-build" ( builtins.readFile  ./scripts/lambda-build.sh );
-  contents = with pkgs; [ coreutils bash wget zip unzip awscli which utillinux script importedPythonPkgs ];
+  contents = with pkgs; [ cacert coreutils curl bash wget zip unzip awscli which utillinux script importedPythonPkgs ];
   importedPythonPkgs = with pkgs; python38.withPackages (pythonPkgs: with pythonPkgs; [
     # other python packages you want
     pip
@@ -16,9 +16,15 @@ pkgs.dockerTools.buildImage {
   #name = "nebulaworks/lambda-build-python";
   name = "drewmullen/lambda-build-python";
   tag = "latest";
+  runAsRoot = ''
+      #!${pkgs.runtimeShell}
+      # ci breaks if it doesnt find a linux release file
+      echo "ID=alpine" > /etc/os-release
+  '';
   config = {
     Env = [
       "PATH=/bin/"
+      "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
     ];
     Labels = {
       "com.nebulaworks.packages" = lib.strings.concatStringsSep "," (lib.lists.naturalSort (lib.lists.forEach contents (x: lib.strings.getName x + ":" + lib.strings.getVersion x)));
